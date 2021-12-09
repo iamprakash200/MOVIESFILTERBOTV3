109353771540fb10ea2e6f44a3c413d6ae8c8667
iamprakash200 <95749054+iamprakash200@users.noreply.github.com>
2021-12-09 14:03:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -71,7 +71,7 @@ async def next_page(bot, query):
 
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("oKda", show_alert=True)
+        return await query.answer("𝚃𝙷𝙸𝚂 𝙸𝚂 𝙽𝙾𝚃 𝙵𝙾𝚁 𝚈𝙾𝚄, 𝚁𝙴𝚀𝚄𝙴𝚂𝚃 𝙰 𝙽𝙴𝚆 𝙾𝙽𝙴 𝙵𝙾𝚁 𝚈𝙾𝚄", show_alert=True)
     try:
         offset = int(offset)
     except:
@@ -178,7 +178,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 except:
                     pass
             else:
-                await query.answer("𝚃𝙷𝙸𝚂 𝙸𝚂 𝙽𝙾𝚃 𝙵𝙾𝚁 𝚈𝙾𝚄 𝚁𝙴𝚀𝚄𝙴𝚂𝚃 𝙰 𝙽𝙴𝚆 𝙾𝙽𝙴 𝙵𝙾𝚁 𝚈𝙾𝚄!!",show_alert=True)
+                await query.answer("This is not for you!!",show_alert=True)
 
 
     elif "groupcb" in query.data:
